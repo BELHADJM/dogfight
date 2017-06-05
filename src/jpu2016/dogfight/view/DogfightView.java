b8769ee1012a22a45cbadc4315d02e869412753a
Mourad7 <mourad.djellouli.algerie@viacesi.fr>
2017-06-05 12:53:39 +0100

    Phase 2

--- src/jpu2016/dogfight/view/DogfightView.java
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -1,5 +1,14 @@
 package jpu2016.dogfight.view;
 
-public class blabla {
+public class DogfightView implements IviewSystem {
+
+	@Override
+	public String displayMessage(String message) {
+		return null;
+	}
+
+	@Override
+	public void closeAll() {		
+	}
 
 }
