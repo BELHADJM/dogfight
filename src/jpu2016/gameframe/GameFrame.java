48b2b15f2248487003f8eaf6f1cec7e06f5a145c
Mourad7 <mourad.djellouli.algerie@viacesi.fr>
2017-06-05 13:49:11 +0100

    Phase 3

---
+++ src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,23 @@
+package jpu2016.gameframe;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import java.awt.event.*;
+
+public class GameFrame implements KeyListener{
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+}
---
+++ src/jpu2016/gameframe/GamePanel.java
@@ -0,0 +1,14 @@
+package jpu2016.gameframe;
+
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class GamePanel implements Observer {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		
+	}
+
+}
